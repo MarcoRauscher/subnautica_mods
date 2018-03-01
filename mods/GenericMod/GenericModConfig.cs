c1aac5ba850eee05c4f58c1c69152e9c3d385480
Marco Rauscher <marco.mr.rauscher@gmail.com>
2018-03-01 03:13:02 +0100

    Added generic mod (template)

---
+++ mods/GenericMod/GenericModConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharedCode.Logging;
+
+namespace GenericMod
+{
+    public class GenericModConfig
+    {
+        public LogLevel LogLevel { get; set; }
+    }
+    }
+}
